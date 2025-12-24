e6aa7e2d0d26548a09a4dc117de94aec2629e1ad
dabbl <dabbler503@gmail.com>
2025-12-24 23:27:48 +0500

    fix

--- src/components/city-favorites/city-favorites.tsx
+++ src/components/city-favorites/city-favorites.tsx
@@ -1,18 +1,33 @@
+import {Link} from 'react-router-dom';
+import {AppRoute, DEFAULT_SORTING} from '../../const.ts';
+import {useAppDispatch} from '../../hooks';
+import {changeCity, changeSorting} from '../../store/settings-process/settings-process';
+import Cities from '../../mocks/cities.ts';
 import {Offer} from '../../types/offer.ts';
 import PlaceCard from '../place-card/place-card.tsx';
-import {Link} from 'react-router-dom';
-import {AppRoute} from '../../const.ts';
 
 type cityFavoritesProps = {
   city: string;
   places: Offer[];
 }
 function CityFavorites({city, places}: cityFavoritesProps): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  const handleCityClick = () => {
+    const cityData = Cities.find(({name}) => name === city);
+    if (!cityData) {
+      return;
+    }
+
+    dispatch(changeCity(cityData));
+    dispatch(changeSorting(DEFAULT_SORTING));
+  };
+
   return (
     <li className="favorites__locations-items">
       <div className="favorites__locations locations locations--current">
         <div className="locations__item">
-          <Link className="locations__item-link" to={AppRoute.Main}>
+          <Link className="locations__item-link" to={AppRoute.Main} onClick={handleCityClick}>
             <span>{city}</span>
           </Link>
         </div>
